5652fb08ec9ae8f5ffa1035b440509aab13ff66c
Daniel <soytruque98@hotmail.es>
2019-10-11 08:58:24 +0200

    update index.php

--- index.php
+++ index.php
@@ -5,22 +5,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
 </head>
 
 <body>
@@ -34,10 +18,6 @@
     $dirint = dir($carpeta);
     $fotos[] = null;
 
-
-
-
-
     $i = 0;
     echo "<form action='delete.php' method='post'>";
     while (($foto = $dirint->read()) !== false) {
@@ -48,10 +28,10 @@
             else
                 $foto[$i] = NULL;
 
-            echo "<li style='float-left; display:inline;'>";
-            echo "<img class='bd-placeholder-img' height = '200px' width='200px' src = '$carpeta/$foto' >";
-            echo "<input class='form-check-input' name='foto[]' value = 'imagenes/$fotos[$i]' type='checkbox'>";
-            echo "</li>";
+            echo "<li style='float-left; display:inline;'>
+            <img class='bd-placeholder-img' height = '200px' width='200px' src = '$carpeta/$foto' >
+            <input class='form-check-input' name='foto[]' value = 'imagenes/$fotos[$i]' type='checkbox'>
+            </li>";
         }
         $i++;
     }
